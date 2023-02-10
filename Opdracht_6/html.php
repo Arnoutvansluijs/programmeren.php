5ae67f9d3fed69d892976cd682174b04aebd9691
Arnoutvansluijs <9018327@student.zadkine.nl>
2023-02-10 10:45:18 +0100

    commit and push

--- Opdracht_6/html.php
+++ Opdracht_6/html.php
@@ -9,11 +9,40 @@
 </head>
 
 <body>
-    <form action="" methode="POST">
+    <form action="" method="POST">
         <label for="input">bedrag exclusief BTW</label>
-        <input type="text" name="input"> </input>
-
+        <input type="text" name="input"></input>
+        <input type="radio" name="BTW" value="9">Laag, 9%
+        <input type="radio" name="BTW" value="21">hoog, 21%
+        <input type="submit" name="verzenden">
     </form>
 </body>
+<?php
+
+if(isset($_POST['verzenden'])){
+    $input = $_POST['input'];
+    $BTW = $_POST['BTW'];
+
+
+if($BTW == 9){
+   $answer = $input * $BTW / 100;
+   $answer2 = $input + $answer; 
+   echo "bedrag inclusief ". "BTW: ". $answer2;
+
+   
+} else {
+    $answer = $input * $BTW / 100;
+   $answer2 = $input + $answer; 
+    echo "bedrag inclusief ". "BTW: ". $answer2;
+   
+}
+
+}
+
+
+
+
+
+?>
 
 </html>
