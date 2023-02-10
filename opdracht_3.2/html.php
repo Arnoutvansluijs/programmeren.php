91b0590a99d74aa8220c24c57dd892e21c3c858b
Arnoutvansluijs <9018327@student.zadkine.nl>
2023-02-10 12:16:01 +0100

    1

---
+++ opdracht_3.2/html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $name = "arnout";
+    echo "$name";
+    $lastname = "van sluijs";
+    echo "<br> $lastname";
+    $number1 = "1";
+    $number2 = "2";
+    echo "<br> $number1 plus $number2";
+
+    ?>
+    
+</body>
+</html>
