acffb3430f5a20ec6c0ab0eb88459bc677a7265b
Arnoutvansluijs <9018327@student.zadkine.nl>
2023-03-21 11:22:09 +0100

    bakpapier

--- Opdracht_p3.6.2/opdracht_6.4.php
+++ Opdracht_p3.6.2/opdracht_6.4.php
@@ -7,6 +7,32 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="" method="post">
+        <label for="radius">straat</label>
+        <input type="number" name="radius" required>
+        <input type="submit" name="submit" value="Berekenen">
+    </form>
+    <?php 
+    if(isset($_POST['submit'])){
+        $r = $_POST['radius'];
+        echo 'de omtrek van een cirkel met straat'. $r . 'is: <br>' . calc_circum($r);
+        echo 'de oppervlakte van een cirkel met straat'. $r . 'is:' . calc_area($r);
+
+
+    }
+
+    function calc_circum($radius){
+        $circum = 2 * pi() * $radius;
+        return round($circum, 2);
+
+    }
+    function calc_area($radius){
+        $area = pi() * $radius * $radius;
+        return round($area, 2);
+
+    }
     
+    
+    ?>
 </body>
 </html>
